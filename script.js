00956b3273d1d2ce9ea2cc0226da0d53ec6ee505
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2023-11-08 23:25:27 +0800

    Add function to play a single round of RPS

--- script.js
+++ script.js
@@ -4,4 +4,30 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
+function playSingleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
+    if (playerSelection === computerSelection) return 'tie';
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return 'You Win! Rock beats Scissors';
+        }
+        return 'You Lose! Paper beats Rock';
+    }
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You Win! Paper beats Rock';
+        }
+        return 'You Lose! Scissors beats Paper';
+    }
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return 'You Win! Scissors beats Paper';
+        }
+        return 'You Lose! Rock beats Scissors';
+    }
+
+    return 'Invalid selection';
+
+}
